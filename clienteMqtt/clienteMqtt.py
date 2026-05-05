24f08476aef75e61b4794cd7582af843487ff56a
BrahianBubans <bubansbrahian@gmail.com>
2026-05-05 12:49:29 -0300

    Se  incluye el nombre de la corrutina  (se implementa %(taskName)s )

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,7 +1,7 @@
 import asyncio, ssl, certifi, logging, os
 import aiomqtt
 
-logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
+logging.basicConfig(format='%(asctime)s - [%(taskName)s] - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
 #Topico  de prueba 1
 async def manejo_prueba1(messages):
@@ -18,19 +18,19 @@ async def manejo_prueba2(messages):
 async def manejo_contador(messages):
    while True:
         await asyncio.sleep(3)
-        lista_conteo["contador"] += 1
-        logging.info("Contador incrementado: " + str(lista_conteo["contador"]))
+        dicc_conteo["contador"] += 1
+        logging.info("Contador incrementado: " + str(dicc_conteo["contador"]))
 
 async def manejo_publicacion(messages):
     topico_c=os.environ['TOPICO_3']
     while True:
         await asyncio.sleep(5)
-        valor=lista_conteo["contador"] 
+        valor=dicc_conteo["contador"] 
         await client.publish(topico_c, payload=str(valor).encode("utf-8"))
         logging.info(f"MQTT publicado en {topico_c}: {valor}")
         
 async def main():
-    s_contador ={"contador": 0}
+    d_contador ={"contador": 0}
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
@@ -48,10 +48,10 @@ async def main():
         await client.subscribe(topico2)
 
         async with asyncio.TaskGroup() as grupo:
-            grupo.create_task(manejo_prueba1(client.messages.filtered(topico1)))
-            grupo.create_task(manejo_prueba2(client.messages.filtered(topico2)))
-            grupo.create_task(manejo_contador(s_contador))
-            grupo.create_task(manejo_publicacion(client,s_contador))
+            grupo.create_task(manejo_prueba1(client.messages.filtered(topico1)),name="ManejoPrueba1")
+            grupo.create_task(manejo_prueba2(client.messages.filtered(topico2)),name="ManejoPrueba2")
+            grupo.create_task(manejo_contador(d_contador),name="ManejoContador")
+            grupo.create_task(manejo_publicacion(client,d_contador),name="ManejoPublicacion")
 
 if __name__ == "__main__":
     asyncio.run(main())
