64629da6964e672fa59ca8b7c34f6696159f0561
BrahianBubans <bubansbrahian@gmail.com>
2026-05-05 16:12:07 -0300

    arreglo de codigo, implementacion de Dispatcher

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,57 +1,68 @@
 import asyncio, ssl, certifi, logging, os
 import aiomqtt
+from dotenv import load_dotenv  
 
+load_dotenv()
 logging.basicConfig(format='%(asctime)s - [%(taskName)s] - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
 #Topico  de prueba 1
-async def manejo_prueba1(messages):
-    async for message in client.messages:
-            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+async def manejo_prueba1(payload):
+        logging.info(f"Topico 1: {payload}")
 
 #Topico de prueba 2
-async def manejo_prueba2(messages):
-   async for message in client.messages:
-            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
-
+async def manejo_prueba2(payload):
+        logging.info(f"Topico 2: {payload}")
 
 #Topico 3 contador
-async def manejo_contador(messages):
-   while True:
+async def manejo_contador(dicc_referencia):
+    while True:
         await asyncio.sleep(3)
-        dicc_conteo["contador"] += 1
-        logging.info("Contador incrementado: " + str(dicc_conteo["contador"]))
+        dicc_referencia["contador"] += 1
+        logging.info(f"Contador incrementado: {dicc_referencia['contador']}")
 
-async def manejo_publicacion(messages):
+async def manejo_publicacion(client, dicc_referencia):
     topico_c=os.environ['TOPICO_3']
     while True:
         await asyncio.sleep(5)
-        valor=dicc_conteo["contador"] 
+        valor=dicc_referencia["contador"] 
         await client.publish(topico_c, payload=str(valor).encode("utf-8"))
         logging.info(f"MQTT publicado en {topico_c}: {valor}")
-        
+
+async def dispatcher(client, t1, t2):
+    async for message in client.messages:
+        payload = message.payload.decode("utf-8")
+        if message.topic.matches(t1):
+            asyncio.create_task(manejo_prueba1(payload), name="Topico1")
+        elif message.topic.matches(t2):
+            asyncio.create_task(manejo_prueba2(payload), name="Topico2")
+
+
 async def main():
+
     d_contador ={"contador": 0}
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
     tls_context.load_default_certs()
 
+    servidor = os.environ.get('SERVIDOR', '').strip()
+    
     async with aiomqtt.Client(
-        os.environ['SERVIDOR'],
+        servidor,
         port=8883,
         tls_context=tls_context,
     ) as client:
-        topico1 = "prueba/uno"
-        topico2 = "prueba/dos"
+        topico1 = os.environ['TOPICO_1']
+        topico2 = os.environ['TOPICO_2']
 
         await client.subscribe(topico1)
         await client.subscribe(topico2)
 
         async with asyncio.TaskGroup() as grupo:
-            grupo.create_task(manejo_prueba1(client.messages.filtered(topico1)),name="ManejoPrueba1")
-            grupo.create_task(manejo_prueba2(client.messages.filtered(topico2)),name="ManejoPrueba2")
-            grupo.create_task(manejo_contador(d_contador),name="ManejoContador")
-            grupo.create_task(manejo_publicacion(client,d_contador),name="ManejoPublicacion")
+            
+            grupo.create_task(dispatcher(client, topico1, topico2), name="Dispatcher")
+            grupo.create_task(manejo_contador(d_contador), name="ManejoContador")
+            grupo.create_task(manejo_publicacion(client, d_contador), name="ManejoPublicacion")
 
 if __name__ == "__main__":
     try:
